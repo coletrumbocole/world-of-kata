c07db5ef69b14576e2ccf68ae17b5cad25e4eb0f
Cole <ctrumbo@circlepix.com>
2017-08-24 04:07:02 +0800

    added an elevator component

--- August/src/app/app.module.ts
+++ August/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ElevatorComponent } from './elevator/elevator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ElevatorComponent
   ],
   imports: [
     BrowserModule
